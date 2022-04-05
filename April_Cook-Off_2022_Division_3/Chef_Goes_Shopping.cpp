cbcf9056fc107586fc6db5b1a0796a0dd4d36fa8
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-04-05 23:26:03 +0530

    Add files via upload

---
+++ April_Cook-Off_2022_Division_3/Chef_Goes_Shopping.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+
+
+int32_t main(){
+
+    int t; cin>>t;
+    while(t--){ 
+        int n; cin>>n;
+        int arrl[n]; int arrr[n]; 
+        for(int i=0; i<n; i++){ 
+            cin>>arrl[i];
+        }
+        for(int i=0; i<n; i++){ 
+            cin>>arrr[i];
+        }
+        int ans=0;
+        for(int i=0; i+1<n; i++){
+            ans += min(arrr[i], arrl[i+1]);
+        }
+
+        cout<<ans<<endl;
+    }
+
+}
